70ac0b0b9f4e4401da9ff0699a4ab97671bc7c8a
javimora0 <jmoralesr@protonmail.com>
2024-03-27 22:32:05 +0100

    Add:  Llamada api externa paises

--- client/src/app/componentes/auth/registro/registro.component.ts
+++ client/src/app/componentes/auth/registro/registro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatFormField, MatFormFieldModule, MatLabel} from "@angular/material/form-field";
@@ -7,6 +7,9 @@ import {MatInput, MatInputModule} from "@angular/material/input";
 import {MatCalendar, MatDatepicker, MatDatepickerInput, MatDatepickerModule} from "@angular/material/datepicker";
 import {MatOption, provideNativeDateAdapter} from "@angular/material/core";
 import {MatSelect} from "@angular/material/select";
+import {PaisesService} from "../../../services/paises.service";
+import {MatAutocomplete, MatAutocompleteTrigger} from "@angular/material/autocomplete";
+import {map, Observable, startWith} from "rxjs";
 
 @Component({
   selector: 'app-registro',
@@ -27,14 +30,15 @@ import {MatSelect} from "@angular/material/select";
     MatDatepickerModule,
     MatOption,
     MatSelect,
-    MatIconButton
+    MatIconButton,
+    MatAutocompleteTrigger,
+    MatAutocomplete
   ],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css',
   providers: [provideNativeDateAdapter()]
 })
-export class RegistroComponent {
-  hide = true;
+export class RegistroComponent implements OnInit {
   formulario_registro = new FormGroup({
     nombre: new FormControl('', [Validators.required]),
     apellidos: new FormControl('', [Validators.required]),
@@ -47,14 +51,29 @@ export class RegistroComponent {
     password: new FormControl('', [Validators.required]),
     repeat_password: new FormControl('', [Validators.required])
   })
+  hide = true;
   boton_vol_pulsado = false;
   boton_org_pulsado = false;
+  paises: any
+
+  constructor(private paises_service: PaisesService) {
+  }
+
+  ngOnInit() {
+    this.paises = this.paises_service.get_paises()
+      .subscribe({
+        next:(res) => {
+          this.paises = res.body
+          console.log(this.paises[0].name)
+        }
+      })
+  }
 
   pulsar_boton(boton: string) {
     if (boton === 'vol') {
       this.boton_vol_pulsado = true
       this.boton_org_pulsado = false
-    }else {
+    } else {
       this.boton_vol_pulsado = false
       this.boton_org_pulsado = true
     }

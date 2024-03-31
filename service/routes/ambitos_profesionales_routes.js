4c906d3c7c3b6d1b56feb3ef5450ac72719e14d1
javimora0 <jmoralesr@protonmail.com>
2024-03-31 21:00:57 +0200

    Fix: todo

--- service/routes/ambitos_profesionales_routes.js
+++ service/routes/ambitos_profesionales_routes.js
@@ -4,7 +4,7 @@ const check_datos = require('/middlewares/check_datos')
 const router = Router()
 
 router.get('', ambitos_controller.get_ambitos)
-//TODO: Proteger la ruta para que tengan token
+//TODO: Proteger la ruta para que sean voluntarios
 router.post('/:id_usuario', check_datos.existe_usuario, ambitos_controller.asignar_ambitos)
 
 module.exports = router

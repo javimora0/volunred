25a0055030b5ffc702f821ca4468fee6becc526e
javimora0 <jmoralesr@protonmail.com>
2024-04-11 15:34:51 +0200

    ADD: Endopoint crear modificar y eliminar derechos o deber

--- service/controllers/derechos_deberes_controller.js
+++ service/controllers/derechos_deberes_controller.js
@@ -10,7 +10,7 @@ const {StatusCodes} = require("http-status-codes");
  */
 const get_derechos = async (req, res = response) => {
     const conx_derechos_deberes = new conexion_deberes_derechos()
-    let derechos = conx_derechos_deberes.get_derechos()
+    let derechos = await conx_derechos_deberes.get_derechos()
     if (!derechos) {
         return res.status(StatusCodes.BAD_REQUEST).json({'msg':'Error al obtener los deberes'})
     }
@@ -25,14 +25,62 @@ const get_derechos = async (req, res = response) => {
  */
 const get_deberes = async (req, res = response) => {
     const conx_derechos_deberes = new conexion_deberes_derechos()
-    let deberes = conx_derechos_deberes.get_deberes()
+    let deberes = await conx_derechos_deberes.get_deberes()
     if (!deberes) {
         return res.status(StatusCodes.BAD_REQUEST).json({'msg':'Error al obtener los deberes'})
     }
     res.status(StatusCodes.OK).json({'deberes':deberes})
 }
 
+/**
+ * @desc Crea un derecho/deber
+ * @param req
+ * @param res
+ * @returns {Promise<e.Response<any, Record<string, any>>>}
+ */
+const crear_derecho_deber = async (req, res = response) => {
+    const conx_derechos_deberes = new conexion_deberes_derechos()
+    let derecho_deber = await conx_derechos_deberes.crear_derecho_deber(req.body)
+    if (!derecho_deber) {
+        return res.status(StatusCodes.BAD_REQUEST).json({'msg':'Error al crear el derecho o deber'})
+    }
+    res.status(StatusCodes.CREATED).json({'derecho_deber':derecho_deber})
+}
+
+/**
+ * @desc Modica un deberecho o deber
+ * @param req
+ * @param res
+ * @returns {Promise<e.Response<any, Record<string, any>>>}
+ */
+const modificar_derecho_deber = async (req, res = response) => {
+    const conx_derechos_deberes = new conexion_deberes_derechos()
+    let derecho_deber = await conx_derechos_deberes.modificar_derecho_deber(req.body,req.params.id)
+    if (!derecho_deber) {
+        return res.status(StatusCodes.BAD_REQUEST).json({'msg':'Error al modificar el derecho o deber'})
+    }
+    res.status(StatusCodes.OK).json({'derecho_deber':derecho_deber})
+}
+
+/**
+ * @desc Soft delete de un derecho o deber
+ * @param req
+ * @param res
+ * @returns {Promise<e.Response<any, Record<string, any>>>}
+ */
+const eliminar_derecho_deber = async (req, res = response) => {
+    const conx_derechos_deberes = new conexion_deberes_derechos()
+    let derecho_deber = await conx_derechos_deberes.eliminar_derecho_deber(req.params.id)
+    if (!derecho_deber) {
+        return res.status(StatusCodes.BAD_REQUEST).json({'msg':'Error al modificar el derecho o deber'})
+    }
+    res.status(StatusCodes.OK).json({'msg':'Derecho o deber eliminado correctamente'})
+}
+
 module.exports = {
     get_deberes,
-    get_derechos
+    get_derechos,
+    crear_derecho_deber,
+    modificar_derecho_deber,
+    eliminar_derecho_deber
 }
--- service/database/ConexionDerechosDeberes.js
+++ service/database/ConexionDerechosDeberes.js
@@ -4,6 +4,55 @@ const conx = new Conexion()
 
 class ConexionDerechosDeberes {
     /**
+     * @desc Crea un derecho/deber
+     * @param body
+     * @returns {Promise<Model<any, TModelAttributes>>}
+     */
+    crear_derecho_deber = async (body) => {
+        conx.conectar()
+        let derecho_deber
+        body.activo = true
+        try {
+            derecho_deber = await model.derecho_deber.create(body)
+        } catch (err) {
+            derecho_deber = null
+        } finally {
+            conx.desconectar()
+        }
+        return derecho_deber
+    }
+
+    modificar_derecho_deber = async (body, id) => {
+        conx.conectar()
+        let derecho_deber
+        body.activo = true
+        try {
+            await model.derecho_deber.update(body, {where:{id:id, activo:true}})
+            derecho_deber = await model.derecho_deber.findByPk(id)
+        } catch (err) {
+            derecho_deber = null
+        } finally {
+            conx.desconectar()
+        }
+        return derecho_deber
+    }
+
+    eliminar_derecho_deber = async (id) => {
+        conx.conectar()
+        let derecho_deber
+        body.activo = true
+        try {
+            await model.derecho_deber.update({activo:false}, {where:{id:id, activo:true}})
+            derecho_deber = await model.derecho_deber.findByPk(id)
+        } catch (err) {
+            derecho_deber = null
+        } finally {
+            conx.desconectar()
+        }
+        return derecho_deber
+    }
+
+    /**
      * @desc Obtiene todos los derechos
      * @returns {Promise<Model[]>}
      */
@@ -45,6 +94,22 @@ class ConexionDerechosDeberes {
             conx.desconectar()
         }
         return deber
+    }
+
+    get_tipo_derecho_deber = async (id_tipo) => {
+        conx.conectar()
+        let tipo = []
+        try {
+            tipo = await model.tipo_derecho_deber.findAll({where: {id: id_tipo,activo:true}})
+        } catch (err) {
+            tipo = null
+        } finally {
+            conx.desconectar()
+            if (tipo.length === 0) {
+                throw new CustomError('No existe el tipo de entrada')
+            }
+        }
+        return tipo_entrada
     }
 }
 

4867b3c84f19295d951100b7adc151e5bbdd31d4
javimora0 <jmoralesr@protonmail.com>
2024-04-30 11:52:27 +0200

    add: todo

--- service/routes/usuarios_routes.js
+++ service/routes/usuarios_routes.js
@@ -6,6 +6,8 @@ const {check} = require("express-validator");
 const validar_jwt = require("../middlewares/validar_jwt");
 const router = Router()
 
+//TODO: Añadir validaciones
+
 router.route('/:id_usuario')
     .get(validar_jwt.validar_token,middleware.existe_usuario ,usuario_controller.get_datos)
 
